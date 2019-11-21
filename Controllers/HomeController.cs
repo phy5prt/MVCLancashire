683b15dfe0d20aeeca5419c8d285b70a80612429
phy5prt <philip.r.tate@gmail.com>
2019-11-21 19:30:59 +0000

    Were winning

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +10,22 @@ namespace MVCLancashire.Controllers
 {
     public class HomeController : Controller
     {
+        ObjectCache cache = MemoryCache.Default;
+        List<Customer> customers;
+
+        public HomeController() {
+
+            customers = cache["customers"] as List<Customer>;
+            if (customers == null) {
+                customers = new List<Customer>();
+            }
+        }
+        public void SaveCache()
+        {
+            cache["customers"] = customers;
+        }
+
+
         public ActionResult Index()
         {
             return View();
@@ -25,20 +42,57 @@ namespace MVCLancashire.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            return View(); }
+        //public ActionResult ViewCustomer(string Name, string Telephone) {
+        public ActionResult ViewCustomer(string id)
+        
+ {
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null) {
+                return HttpNotFound();
+                    }
+            else {
+                return View(customer);
+            }
+
+
+          //  //guid is random unique number computer generates useful in dbs
+          //  customer.Id = Guid.NewGuid().ToString();
+          //  // customer.Name = Name;
+          //  customer.Name = postedCustomer.Name;
+          // // customer.Telephone = Telephone;
+          //  customer.Telephone = postedCustomer.Telephone;
+
+         //   return View(customer);
+
         }
 
-        //public ActionResult ViewCustomer(string Name, string Telephone) {
-        public ActionResult ViewCustomer(Customer postedCustomer) {
-          Customer customer = new Customer();
-            //guid is random unique number computer generates useful in dbs
-            customer.Id = Guid.NewGuid().ToString();
-            // customer.Name = Name;
-            customer.Name = postedCustomer.Name;
-           // customer.Telephone = Telephone;
-            customer.Telephone = postedCustomer.Telephone;
-            return View(customer);
+        public ActionResult EditCustomer(string id) {
 
+            Customer customer = customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+            { return HttpNotFound(); }
+            else {
+                return View(customer);
+            }
+        }
+        [HttpPost]
+        public ActionResult EditCustomer(Customer customer, string Id)
+        {
+            var customerToEdit = customers.FirstOrDefault(c => c.Id == Id);
+           if (customer == null) {
+                return HttpNotFound();
+            }
+            else 
+            {
+                customerToEdit.Name = customer.Name;
+                customerToEdit.Telephone = customer.Telephone;
+                SaveCache();
+
+                return RedirectToAction("CustomerList");
+           
+
+            }
         }
 
         public ActionResult AddCustomer() {
@@ -46,12 +100,55 @@ namespace MVCLancashire.Controllers
 
         }
 
+        [HttpPost]
+        public ActionResult AddCustomer(Customer customer) {
+
+            if(!ModelState.IsValid) {
+                return View(customer);
+            }
+            customer.Id = Guid.NewGuid().ToString();
+            customers.Add(customer);
+            SaveCache();
+
+            return RedirectToAction("CustomerList");
+
+        }
+
+        public ActionResult DeleteCustomer(string Id)
+        {
+           Customer customer = customers.FirstOrDefault(c => c.Id == Id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(customer);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("DeleteCustomer")]
+        public ActionResult ConfirmDeleteCustomer(string Id)
+        {
+            Customer customer = customers.FirstOrDefault(c => c.Id == Id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                customers.Remove(customer);
+                return RedirectToAction("CustomerList");
+            }
+        }
+
         public ActionResult CustomerList() {
 
-            List<Customer> customers = new List<Customer>();
+            //List<Customer> customers = new List<Customer>();
 
-            customers.Add(new Customer() { Name = "Fred", Telephone="12345"   });
-            customers.Add(new Customer() { Name = "Blobby", Telephone = "000004" });
+            //customers.Add(new Customer() { Name = "Fred", Telephone="12345"   });
+            //customers.Add(new Customer() { Name = "Blobby", Telephone = "000004" });
             return View(customers);
           
         }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace MVCLancashire.Models
 {
@@ -9,7 +11,10 @@ namespace MVCLancashire.Models
     {
 
         public string Id { get; set; }
-        public string Name { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage ="long string")]
+        [DisplayName("Enter Your Name")]
+        public string Name { get; set; } 
         public string Telephone { get; set; }
     }
 
